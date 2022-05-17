0b2bba3fd8309a1e3c9d93b09eefb32332815d28
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-05-17 20:17:00 +0900

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -52,6 +52,24 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    
 apt-get update
 apt-get install -y nvidia-docker2
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "default-runtime": "nvidia",
+   "runtimes": {
+      "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+      }
+   }
+}
+EOF
    
 systemctl daemon-reload
 systemctl restart docker
