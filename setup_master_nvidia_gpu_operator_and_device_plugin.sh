00474e0483acb5e6faf32caf779f5bab89dbbe5e
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-05-18 18:30:48 +0900

    Update setup_master_nvidia_gpu_operator_and_device_plugin.sh

--- setup_master_nvidia_gpu_operator_and_device_plugin.sh
+++ setup_master_nvidia_gpu_operator_and_device_plugin.sh
@@ -13,15 +13,6 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
   && helm repo update
 
-# this is for the case with pre-installed nvidia driver and nvidia docker
-# if you are under different circumstances, please refer to the official site of installing NVIDIA GPU operator
-# https://docs.nvidia.com/datacenter/cloud-native/gpu-operator/getting-started.html#install-nvidia-gpu-operator
-helm install --wait --generate-name \
-  -n gpu-operator --create-namespace \
-  nvidia/gpu-operator \
-  --set driver.enabled=false \
-  --set toolkit.enabled=false
-
 #------------- install NVIDIA device plugin
 
 # add nvidia device plugin helm repository
