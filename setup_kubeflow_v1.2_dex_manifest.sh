3999c808a25b5aaac7b41c1d61db3382370b9bc8
master <lionmini@naver.com>
2022-05-19 10:45:15 +0000

    .

---
+++ setup_kubeflow_v1.2_dex_manifest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Turn off ufw or open a specific port for kubeflow installation
+
+#------------- install kfctl which is a CLI for deploy and manage kubeflow components
+wget https://github.com/kubeflow/kfctl/releases/download/v1.2.0/kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
+
+tar xzvf kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
+mv kfctl /usr/bin
+
+#------------- create directory to store scripts && set distribution template
+export KF_NAME=handson-kubeflow
+export BASE_DIR=/home/${USER}
+export KF_DIR=${BASE_DIR}/${KF_NAME}
+export CONFIG_URI="https://raw.githubusercontent.com/kubeflow/manifests/v1.2-branch/kfdef/kfctl_istio_dex.v1.2.0.yaml"
+
+#------------- build kustomize packages
+mkdir -p ${KF_DIR}
+cd ${KF_DIR}
+kfctl build -V -f ${CONFIG_URI}
+
+#------------- execute packages using kfctl
+export CONFIG_FILE=${KF_DIR}/kfctl_istio_dex.v1.2.0.yaml
+kfctl apply -V -f ${CONFIG_FILE}
