6f15c8a3c98686e3438a87d9dbc690319798fc43
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-05-20 14:25:54 +0900

    Update setup_join_master.sh

--- setup_join_master.sh
+++ setup_join_master.sh
@@ -54,7 +54,7 @@ apt-get update
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet=1.20.5-00 kubeadm=1.20.5-00 kubectl=1.20.5-00
+apt-get install -y kubelet=1.20.11-00 kubeadm=1.20.11-00 kubectl=1.20.11-00
 apt-mark hold kubelet kubeadm kubectl
 
 #------------- enable kubectl & kubeadm auto-completion
