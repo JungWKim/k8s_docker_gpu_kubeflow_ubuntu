abede4336aebe3b7aef3f85a4c88759dccf4a5ac
JungWKim <lionmini@naver.com>
2022-06-27 11:40:56 +0900

    add nfs-provisioner script

---
+++ setup_nfs_provisioner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p -e "Enter the nfs server's IP: " nfs_ip
+read -p -e "Enter the nfs path to install db files: (ex. /volume1/kubeflow ) " nfs_path
+
+#------------- add nfs provisioner repository
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
+#------------- install nfs provisioner
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=${nfs_ip} \
+    --set nfs.path=${nfs_path}
+
+#------------- set nfs-client as default storage class
+kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
