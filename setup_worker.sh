32cfa400bf4efc2be0d2b6e6f6aff78418ba57c1
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-07-03 00:48:58 +0900

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -55,12 +55,11 @@ systemctl restart docker
 #-------------- install nvidia docker(nvidia container toolkit)
 if [ ${answer} = yes ] || [ ${answer} = y ] ; then
 
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-	   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-	   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-	   
-	apt-get update
-	apt-get install -y nvidia-docker2
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+	curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+	curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+ 	apt update && apt install -y nvidia-container-toolkit
+	systemctl restart docker
 
 	cat > /etc/docker/daemon.json <<EOF
 {
